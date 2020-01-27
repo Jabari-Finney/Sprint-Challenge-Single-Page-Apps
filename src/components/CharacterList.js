11ce0e56fa06c1642ab022ca689e14c6ea68ee63
unknown <jabaritf@gmail.com>
2020-01-27 18:08:54 -0500

    added my axios call and tracked data with use state

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,12 +1,17 @@
 import React, { useEffect, useState } from "react";
+import Axios from 'axios';
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState({})
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
+    Axios.get('https://rickandmortyapi.com/api/character/')
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
+}, [])
 
   return (
     <section className="character-list">
