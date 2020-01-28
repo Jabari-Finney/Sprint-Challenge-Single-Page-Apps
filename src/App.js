aa6819e4a9a3f80a11e08b04db6d51fb9ebb8b52
unknown <jabaritf@gmail.com>
2020-01-27 19:14:35 -0500

    added restarted project by copying origninal master branch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 import Header from "./components/Header.js";
 import WelcomePage from './components/WelcomePage'
+import CharacterList from './components/CharacterList'
 import { Route } from "react-router-dom";
+
 
 
 export default function App() {
@@ -9,6 +11,7 @@ export default function App() {
     <main>
       <Header />
       <Route exact path='/' component={WelcomePage} />
+      <Route path='/characters' component={CharacterList} />
     </main>
   );
 }
