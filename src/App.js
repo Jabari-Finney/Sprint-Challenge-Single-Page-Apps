0a61f3b881624bccc11d4aabaec9ec9b5bd10aa0
unknown <jabaritf@gmail.com>
2020-01-27 19:48:41 -0500

    got character cards to appear with image and name, utilized api rimport React from react;equest to call get data for cards

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from "react";
 import Header from "./components/Header.js";
-import WelcomePage from './components/WelcomePage'
-import CharacterList from './components/CharacterList'
-import { Route } from "react-router-dom";
+import CharacterList from "./components/CharacterList";
+import WelcomePage from "./components/WelcomePage";
 
-
+import { Route, Link } from 'react-router-dom';
 
 export default function App() {
   return (
     <main>
       <Header />
-      <Route exact path='/' component={WelcomePage} />
-      <Route path='/characters' component={CharacterList} />
+      <Link to='/'>Home</Link>
+      <Link to='/characters'>Characters</Link>
+
+      <Route exact path='/' component={WelcomePage}/>
+      <Route path='/characters' component={CharacterList}/>
     </main>
   );
 }
--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,3 +1,32 @@
 import React, { useEffect, useState } from "react";
+import Axios from 'axios'
+import LocationCard from './LocationCard'
 
-export default function LocationsList() {}
+export default function LocationsList() {
+
+//hook
+
+const [locations, setLocations] = useState([])
+
+useEffect(() => {
+    Axios
+      .get('https://rickandmortyapi.com/api/location/')
+      .then(res => setLocations(res.data.results))
+      .catch(err => console.log(err))
+  }, [])
+
+  return (
+    <div>
+    {locations &&
+      locations.map(location => (
+        <LocationCard
+            key={location.id}
+            name={location.name}
+            type={location.type}
+            dimension={location.dimension}
+            residents={location.residents}
+          />
+      ))}
+        </div>
+      )
+}
